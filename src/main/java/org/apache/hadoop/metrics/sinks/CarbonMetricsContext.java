e5ad9dae7e40591b52086fd2078382662c00101b
janstenpickle <jansten@pickle.com>
2013-06-13 18:07:39 +0100

    initial commit

---
+++ src/main/java/org/apache/hadoop/metrics/sinks/CarbonMetricsContext.java
@@ -0,0 +1,11 @@
+package org.apache.hadoop.metrics.sinks;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chris
+ * Date: 13/06/2013
+ * Time: 15:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class CarbonMetricsContext {
+}
